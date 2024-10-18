df616953998341becd4ea9e39281d380a2ebb554
TLC <tituslifecharlie@gmail.com>
2024-10-18 01:59:32 +0100

    done

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
 """
-Module for calculating minimum operations.
+Module that calculates the minimum number of operations needed to get exactly n H characters.
 """
-
 
 def minOperations(n):
     """
-    Returns the minimum number of operations needed to reach n 'H' characters
-    :param n: integer, the target number of characters
-    :return: integer, minimum number of operations
+    Returns the minimum number of operations required to get n 'H' characters in the file.
+    
+    :param n: integer, the target number of 'H' characters
+    :return: integer, the minimum number of operations, or 0 if n is less than 2
     """
     if n < 2:
         return 0
-
+    
     operations = 0
     divisor = 2
 
     while n > 1:
-        # While n is divisible by the current divisor
         while n % divisor == 0:
-            operations += divisor  # Add the divisor to operations
-            n //= divisor  # Divide n by the divisor
+            operations += divisor  # Add the divisor to the operation count
+            n //= divisor  # Reduce n by dividing by the divisor
         divisor += 1
-
+    
     return operations
